383761072d78a7d2152bab16625291ff96b3d600
neonerz <gerald@rewrite.media>
2023-01-20 16:44:35 -0500

    Update artemis.ts

--- artemis.ts
+++ artemis.ts
@@ -1,7 +1,19 @@
+//enums
+enum FourDirectionUpDown {
+    //% block="up"
+    Up = SixDirection.Up,
+    //% block="down"
+    Down = SixDirection.Down,
+    //% block="left"
+    Left = SixDirection.Left,
+    //% block="right"
+    Right = SixDirection.Right
+}
+
 // global variables
 const communicationsTimeout = 100;
 const start_game_position = world(0, 7, 0)
-const start_game_block = WHITE_CONCRETE
+const signal_block_type = WHITE_CONCRETE
 
 
 //%  block="Artemis: Moon Journey" weight=200 color=#0B3D91 icon="\u1F680"
@@ -12,7 +24,30 @@ namespace artemis {
      */
     //% block="i'm ready"
     export function startGame(): void{
-        blocks.place(start_game_block, start_game_position)
+        signalBlock(0, 0)
+    }
+    
+    /**
+     * Agent Move Footsize 1
+     */
+    //% block="agent move %d by %n"
+    export function agentMoveFoot1(d: FourDirectionUpDown, n: number): void {
+        agent.move(d, n)
+
+    }
+
+    /**
+     * Agent Move Footsize 3
+     */
+    //% block="agent move %d by %n"
+    export function agentMoveFoot3(d: FourDirectionUpDown, n: number): void {
+        agent.move(d, n * 3)
+
+    }
+
+    // helper functions
+    function signalBlock(activity: number, player: number) : void{
+        blocks.place(signal_block_type, world(player, 7, activity))
         loops.pause(communicationsTimeout)
     }
 
