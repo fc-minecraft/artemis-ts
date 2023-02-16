241efdb799c9f1d8ecda276177e6173806bdd73b
neonerz <gerald@rewrite.media>
2023-02-16 16:02:25 -0500

    Update artemis.ts

--- artemis.ts
+++ artemis.ts
@@ -10,6 +10,67 @@ enum FourDirectionUpDown {
     Right = SixDirection.Right
 }
 
+enum extendArm {
+    //% block="1"
+    a,
+    //% block="2"
+    b,
+    //% block="3"
+    c
+}
+
+enum matrix_y_axis_full {
+    //% block="0"
+    a,
+    //% block="1"
+    b,
+    //% block="2"
+    c,
+    //% block="3"
+    d,
+    //% block="4"
+    e,
+    //% block="5"
+    f,
+    //% block="6"
+    g,
+    //% block="7"
+    h,
+    //% block="8"
+    i,
+    //% block="9"
+    j
+}
+
+enum matrix_x_axis_full {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z
+}
+
 // global variables
 const communicationsTimeout = 100;
 const start_game_position = world(0, 7, 0)
@@ -50,22 +111,13 @@ namespace artemis {
     export function startGameP4(): void {
         signalBlock(0, 0, 4, default_signal_block)
     }
-    
-    /**
-     * Agent Move Footsize 1
-     */
-    //% block="agent move %d by %n"
-    export function agentMoveFoot1(d: FourDirectionUpDown, n: number): void {
-        agent.move(d, n)
-    }
 
     /**
-     * Agent Move Footsize 3
+     * Space Junk (Activity 16) Collect Debris
      */
-    //% block="agent move %d by %n"
-    export function agentMoveFoot3(d: FourDirectionUpDown, n: number): void {
-        agent.move(d, n * 3)
-
+    //% block="collect debris"
+    export function collectDebrisA16(): void {
+        signalBlock(0, 16, 1, default_signal_block)
     }
 
     /**
@@ -87,27 +139,118 @@ namespace artemis {
     /**
      * Lunar Garden 1 (Activity 13)
      */
-    //% block="plant seed 1"
+    //% block="plant seed"
     export function lunarGarden1A13(): void {
         signalBlock(0, 13, 1, default_signal_block)
     }
 
     /**
-     * Lunar Garden 2 (Activity 13)
+     * Water Recycling (Activity 20)
      */
-    //% block="plant seed 2"
-    export function lunarGarden2A13(): void {
-        signalBlock(1, 13, 1, default_signal_block)
+    //% block="clear debris"
+    export function clearDebrisA20(): void {
+        signalBlock(0, 20, 1, default_signal_block)
+    }
+
+
+    /**
+     * ISS Help (Activity 21) Load Cargo
+     */
+    //% block="load cargo"
+    export function loadCargoA21(): void {
+        signalBlock(0, 21, 1, default_signal_block)
     }
 
     /**
-     * Lunar Garden 3 (Activity 13)
+     * Agent Move Footsize 1
      */
-    //% block="plant seed 3"
-    export function lunarGarden3A13(): void {
-        signalBlock(2, 13, 1, default_signal_block)
+    //% block="agent move %d by %n"
+    export function agentMoveFoot1(d: FourDirectionUpDown, n: number): void {
+        agent.move(d, n)
     }
 
+    /**
+     * Agent Move Footsize 3
+     */
+    //% block="agent move %d by %n"
+    export function agentMoveFoot3(d: FourDirectionUpDown, n: number): void {
+        agent.move(d, n * 3)
+
+    }
+
+    /**
+     * Space Junk (Activity 16) ADRV Move
+     */
+    //% block="ADRV move %d by %n"
+    export function adrvMoveA16(d: FourDirectionUpDown, n: number): void {
+        agent.move(d, n)
+    }
+
+    /**
+     * Asteroid Mining (Activity 12) Rover Move
+     */
+    //% block="rover move %d by %n"
+    export function roverMoveA12(d: FourDirectionUpDown, n: number): void {
+        agent.move(d, n)
+    }
+
+
+    /**
+     * Mars Recon (Activity 11) Rover Move
+     */
+    //% block="rover move %d by %n"
+    export function roverMoveA11(d: FourDirectionUpDown, n: number): void {
+        agent.move(d, n*3)
+    }
+
+    /**
+     * Aurora Pictures (Activity 14)
+     */
+    //% block="take picture at $x_axis $y_axis"
+    export function takePictureA14(x_axis: matrix_x_axis_full, y_axis: matrix_y_axis_full): void {
+        signalBlock(0, 14, 1, default_signal_block)
+    }
+
+    /**
+     * Orion Splashdown (Activity 15)
+     */
+    //% block="set location to $x_axis $y_axis"
+    export function setLocationA15(x_axis: matrix_x_axis_full, y_axis: matrix_y_axis_full): void {
+        signalBlock(0, 15, 1, default_signal_block)
+    }
+
+    /**
+     * Landing Site (Activity 17)
+     */
+    //% block="mark location to $x_axis $y_axis"
+    export function markLocationA17(x_axis: matrix_x_axis_full, y_axis: matrix_y_axis_full): void {
+        signalBlock(0, 17, 1, default_signal_block)
+    }
+
+    /**
+     * Earth's Pollution (Activity 18)
+     */
+    //% block="deploy CubeSat to $x_axis $y_axis"
+    export function deployCubsatA18(x_axis: matrix_x_axis_full, y_axis: matrix_y_axis_full): void {
+        signalBlock(0, 18, 1, default_signal_block)
+    }
+
+    /**
+     * Laser Alignment (Activity 19)
+     */
+    //% block="aim at $x_axis $y_axis"
+    export function aimAtA18(x_axis: matrix_x_axis_full, y_axis: matrix_y_axis_full): void {
+        signalBlock(0, 18, 1, default_signal_block)
+    }
+
+
+    /**
+     * ISS Help (Activity 21) Extend Arm
+     */
+    //% block="extend arm $extendArm"
+    export function extendArmA21(extendArm: extendArm): void {
+        signalBlock(0, 21, 1, default_signal_block)
+    }
 
     // helper functions
     function signalBlock(step: number, activity: number, player: number, block: Block) : void{
